6a022b341e61193da8fca0bdaa75dd310bea4ff7
Ritika <69005146+rit-iika@users.noreply.github.com>
2021-06-20 16:39:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.post("/submit", (req, res) => {
   res.redirect("/display.html");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT ||3000, () => {
   console.log("Listening on port", 3000);
 });
