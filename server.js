405b08e4aefba0fcef0ecd6eb5ec23c23d335de0
Ritika <69005146+rit-iika@users.noreply.github.com>
2021-06-20 16:48:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.post("/submit", (req, res) => {
   res.redirect("/display.html");
 });
 
-app.listen(Process.env.PORT ||3000, () => {
-  console.log("Listening on port", 3000);
+// app.listen(Process.env.PORT ||3000, () => {
+//   console.log("Listening on port", 3000);
+// });
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
